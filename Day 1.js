9497bf6c7dae22d3cc917b0233e8aade028b8278
Fadi960 <fa1712320@gmail.com>
2024-05-08 12:26:10 +0500

    day1

---
+++ Day 1.js
@@ -0,0 +1,6 @@
+const newName = (updateName, name) => {
+    console.log(name);
+    updateName("Ahmed");
+
+};
+module.exports = newName;
---
+++ main.js
@@ -0,0 +1,9 @@
+const newName = require("./Day 1");
+let name = "fahad"
+
+const updateName = (newValue) => {
+    name = newvalue;
+};
+
+newName(updateName, name);
+console.log(name);
